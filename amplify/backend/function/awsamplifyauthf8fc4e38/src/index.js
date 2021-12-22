464afd9bb001548eb9f38485e823136a2910a9e3
Alex Vladut <alexandruvladut@gmail.com>
2021-12-22 15:19:03 +0200

    prevent creating membership on organisation item update

--- amplify/backend/function/awsamplifyauthf8fc4e38/src/index.js
+++ amplify/backend/function/awsamplifyauthf8fc4e38/src/index.js
@@ -17,28 +17,30 @@ const USER_MEMBERSHIP_TABLE_NAME =
 exports.handler = async (event) => {
   console.log(JSON.stringify(event, null, 2));
   await Promise.all(
-    event.Records.map(async (record) => {
-      console.log(record.eventID);
-      console.log(record.eventName);
-      console.log("DynamoDB Record: %j", record.dynamodb);
+    event.Records.filter((record) => record.eventName === "INSERT").map(
+      async (record) => {
+        console.log(record.eventID);
+        console.log(record.eventName);
+        console.log("DynamoDB Record: %j", record.dynamodb);
 
-      await dynamoDb.send(
-        new PutItemCommand({
-          TableName: USER_MEMBERSHIP_TABLE_NAME,
-          Item: marshall({
-            __typename: "UserMembership",
-            id: uuid(),
-            organisationId: record.dynamodb.NewImage.id.S,
-            userId: record.dynamodb.NewImage.createdById.S,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            _version: 1,
-            _lastChangedAt: Date.now(),
-          }),
-          ConditionExpression: "attribute_not_exists(id)",
-        })
-      );
-    })
+        await dynamoDb.send(
+          new PutItemCommand({
+            TableName: USER_MEMBERSHIP_TABLE_NAME,
+            Item: marshall({
+              __typename: "UserMembership",
+              id: uuid(),
+              organisationId: record.dynamodb.NewImage.id.S,
+              userId: record.dynamodb.NewImage.createdById.S,
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString(),
+              _version: 1,
+              _lastChangedAt: Date.now(),
+            }),
+            ConditionExpression: "attribute_not_exists(id)",
+          })
+        );
+      }
+    )
   );
   return Promise.resolve("Successfully processed DynamoDB record");
 };
